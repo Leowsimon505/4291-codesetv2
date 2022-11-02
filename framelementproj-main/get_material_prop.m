37c43d5377341896165ae4c5e4932406e099038e
chiewwl <117244995+chiewwl@users.noreply.github.com>
2022-11-02 10:57:28 +0800

    Update get_material_prop.m

--- framelementproj-main/get_material_prop.m
+++ framelementproj-main/get_material_prop.m
@@ -4,13 +4,13 @@ function val = get_material_prop(prop_type, elem_type)
 material_prop = zeros(4,2);
 
 % properties for element type 1
-material_prop(1,1) = 78.5e9;                  % Young's Modulus
+material_prop(1,1) = 210e9;                  % Young's Modulus
 material_prop(2,1) = 1.5;                    % element width
 material_prop(3,1) = 0.5;                    % element height
 material_prop(4,1) = 19300;                   % density
 
 % properties for element type 2
-material_prop(1,2) = 78.5e9;             % Young's Modulus
+material_prop(1,2) = 210e9;             % Young's Modulus
 material_prop(2,2) = 0.6;                 % element width
 material_prop(3,2) = 0.3;               % element height
 material_prop(4,2) = 19300;          % density
