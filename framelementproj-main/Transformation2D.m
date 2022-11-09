060836aa038a26a822afbec4379aed228e2b855a
chiewwl <117244995+chiewwl@users.noreply.github.com>
2022-11-09 23:56:43 +0800

    Delete framelementproj-main directory

--- framelementproj-main/Transformation2D.m
+++
@@ -1,9 +0,0 @@
-function A = Transform2D(theta,dir)
-%Global to Local, dir = 1
-if (dir == 1)
-    A = [cos(theta), sin(theta); -sin(theta) , cos(theta)];
- %Local to Global, dir = 0
-elseif (dir == 0)
-    A = [cos(theta), -sin(theta); sin(theta) , cos(theta)];
-   
-end
