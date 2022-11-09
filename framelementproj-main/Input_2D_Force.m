f9772a16ecedfb9160351bb71d53d24902bbe595
chiewwl <117244995+chiewwl@users.noreply.github.com>
2022-11-09 23:42:46 +0800

    Update Input_2D_Force.m

--- framelementproj-main/Input_2D_Force.m
+++ framelementproj-main/Input_2D_Force.m
@@ -1,9 +1,5 @@
-function global_f = Input_2D_Force(nodes,elems)
+function global_f = Input_2D_Force(nodes,elems, b, h)
 
-%nodes = load('Input_node_coord_truss.txt');
-%elems = load('Input_elem_connect_truss.txt');
-
-%[nodes,elems] = elementsplit(nodes,elems,5);
 %  Determine the number of nodes
 node_size = size(nodes);
 num_of_nodes = node_size(1);
@@ -14,15 +10,10 @@ num_of_elem = elem_size(1);
 
 % Create space for vertical force
 force_Y = zeros(num_of_nodes,1);
-force_Y(3) = -52205*9.81;
-
-% Create space for horizontal force
-%force_X = zeros(num_of_nodes,1);
-%force_X(5) = -10000;
+force_Y(3) = -52205*9.81/2;
 
 % Create space for Moment
 moment = zeros(num_of_nodes,1);
-% moment(node) = ???;
 global_f = zeros(num_of_nodes*3,1);
 
 for j = 1:num_of_elem
@@ -31,8 +22,6 @@ for j = 1:num_of_elem
     node_1 = elems(j,1);
     node_2 = elems(j,2);
 
-    elem_type = elems(j,3);
-
     node_1_coord = nodes(node_1,:);
     node_2_coord = nodes(node_2,:);
 
@@ -40,8 +29,8 @@ for j = 1:num_of_elem
     L = ((node_2_coord(1)-node_1_coord(1))^2 + (node_2_coord(2)-node_1_coord(2))^2)^0.5;
 
     % Material Properties
-    density = get_material_prop('p',elem_type);
-    area = get_material_prop('b', elem_type) * get_material_prop('h', elem_type);
+    density = get_material_prop('p');
+    area = b * h;
 
     % Calculate half element weight
     weight = area * L * density * 9.81 / 2;
@@ -56,6 +45,5 @@ end
 
 for i=1:num_of_nodes
     global_f(3*i - 1) = force_Y(i);
-    %global_f(3*i -2) = force_X(i);
     global_f(3*i) = moment(i);
 end
