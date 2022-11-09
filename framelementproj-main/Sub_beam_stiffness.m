f9151f862ce058b04f59cc060b4195c8bae99558
chiewwl <117244995+chiewwl@users.noreply.github.com>
2022-11-09 23:44:10 +0800

    Update Sub_beam_stiffness.m

--- framelementproj-main/Sub_beam_stiffness.m
+++ framelementproj-main/Sub_beam_stiffness.m
@@ -1,17 +1,17 @@
-function Ke = Sub_beam_stiffness(node_1_coord, node_2_coord, elem_type)
+function Ke = Sub_beam_stiffness(node_1_coord, node_2_coord, b, h)
 %   Returns a 4 by 4 element stiffness matrix
 %   Inputs: global x-coordinates of node 1 and node 2
 %   Requires E & A function for beam
   
   % Load Young's Modulus
-  E = get_material_prop('E', elem_type);
+  E = get_material_prop('E');
 
   % Obtain the length of element
   L = sqrt((node_2_coord(1)-node_1_coord(1))^2 + (node_2_coord(2)-node_1_coord(2))^2);
 
   % Load moment of inertia
-  b = get_material_prop('b', elem_type);
-  h = get_material_prop('h', elem_type);
+%   b = get_material_prop('b');
+%   h = get_material_prop('h');
   I = b * h^3 / 12;
   
   % For Gauss quadrature sum
