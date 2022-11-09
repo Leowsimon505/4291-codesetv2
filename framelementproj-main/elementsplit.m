cc04042e220b4ed6df35b353b0b97608d56d6d3f
chiewwl <117244995+chiewwl@users.noreply.github.com>
2022-11-09 23:40:22 +0800

    Update elementsplit.m

--- framelementproj-main/elementsplit.m
+++ framelementproj-main/elementsplit.m
@@ -9,7 +9,7 @@ if iters >= 1
     elem_size = size(elems);
     num_of_elem = elem_size(1);
     snode = zeros((num_of_elem +num_of_nodes),2);
-    selem = zeros(2*num_of_elem,3);
+    selem = zeros(2*num_of_elem,2);
     for j = 1:num_of_nodes
         snode(j,1) = nodes(j,1);
         snode(j,2) = nodes(j,2);
@@ -21,13 +21,10 @@ if iters >= 1
         snode((c),2)= nodes(elems(i,1),2)+(nodes(elems(i,2),2) - nodes(elems(i,1),2))/2;
         selem(step,1) = elems(i,1);
         selem(step,2) = i +num_of_nodes;
-        selem(step,3) = elems(i,3);
         selem(step+1,1) = i+ num_of_nodes;
         selem(step+1,2) = elems(i,2);
-        selem(step+1,3) = elems(i,3);
     end
     [snode,selem]= elementsplit(snode,selem,iters-1);
 end
     
 end
-        
